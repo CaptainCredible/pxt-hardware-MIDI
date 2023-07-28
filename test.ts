e78e63a9b0f30d076105f0f2ccafc64cd7cedff7
CaptainCredible <cpt.credible@gmail.com>
2023-07-28 21:31:46 +0200

    Added test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,15 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/*
+Test sketch
+Sends midi note on messages followed by noteOff messages from pin P1
+*/
+midiInOut.setMidiOutPin(SerialPin.P1)
+basic.forever(function() {
+    basic.showIcon(IconNames.Heart)
+    let noteToSend = randint(50, 70) // select a random note number to send
+    let noteDuration = 100 //set a duration of 100ms
+    let velocityToSend = 127 // maximum velocity
+    let midiChannelToUse = 1 // channels 1 - 16 available
+    midiInOut.sendNote(noteToSend, velocityToSend, noteDuration, midiChannelToUse)
+    basic.showIcon(IconNames.SmallHeart)
+})
+
